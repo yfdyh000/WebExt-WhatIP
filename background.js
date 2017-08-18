75168d3ba526f3a16f32c9ee097ba37ffe8e53a2
YFdyh000 <yfdyh000@gmail.com>
2017-08-19 04:08:31 +0800

    refine format

--- background.js
+++ background.js
@@ -7,56 +7,56 @@ var ipv6 = /(?:[\w*]:)*:[\w*]/;
 // Handlers
 function handleRequest(requestInfo) {
   // Display ip as title (tooltip)
-  browser.pageAction.setTitle({tabId: requestInfo.tabId, title: requestInfo.ip});
+  browser.pageAction.setTitle({ tabId: requestInfo.tabId, title: requestInfo.ip });
 
   // Changing icon whether it's IPv4
-  if(ipv4.test(requestInfo.ip)) {
-    if(log)
+  if (ipv4.test(requestInfo.ip)) {
+    if (log)
       console.log("ipv4");
     browser.pageAction.setIcon({
       tabId: requestInfo.tabId,
-	  path: {
+      path: {
         16: "icons/v4-16.png",
-	    32: "icons/v4-32.png"
-	  }
+        32: "icons/v4-32.png"
+      }
     });
   }
   // Or IPv6
-  else if(ipv6.test(requestInfo.ip)) {
-    if(log)
+  else if (ipv6.test(requestInfo.ip)) {
+    if (log)
       console.log("ipv6");
     browser.pageAction.setIcon({
       tabId: requestInfo.tabId,
-	  path: {
+      path: {
         16: "icons/v6-16.png",
-	    32: "icons/v6-32.png"
-	  }
+        32: "icons/v6-32.png"
+      }
     });
   }
   // Or neither
   else {
-    if(log)
+    if (log)
       console.log("Unrecognized IP");
     browser.pageAction.setIcon({
       tabId: requestInfo.tabId,
-	  path: {
+      path: {
         16: "icons/vX-16.png",
-	    32: "icons/vX-32.png"
-	  }
+        32: "icons/vX-32.png"
+      }
     });
   }
-  
+
   // Toggle icon if it has dissapeared
   browser.pageAction.show(requestInfo.tabId);
 }
 
 function handleActivated(activeInfo) {
   // Toggle Icon so it keeps showing on active tab
-  if(log)
+  if (log)
     console.log("Toggle icon on active tab: " + activeInfo.tabId);
   browser.pageAction.show(activeInfo.tabId);
 }
 
 // Listeners
-browser.webRequest.onCompleted.addListener(handleRequest, {urls: ["<all_urls>"], types: ["main_frame"]});
+browser.webRequest.onCompleted.addListener(handleRequest, { urls: ["<all_urls>"], types: ["main_frame"] });
 browser.tabs.onActivated.addListener(handleActivated);
